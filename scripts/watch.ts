de64b4e97ed5120362832d4ae54c3c5a2c83cef3
Ami <ami@ccrsxx.com>
2025-10-26 11:44:55 +0700

    fix: watch script not running

--- scripts/watch.ts
+++ scripts/watch.ts
@@ -1,9 +1,11 @@
 import { executeSassJob } from './utils.ts';
 
-export function watchThemes(): void {
+export function main(): void {
   console.log('Starting theme watch process...');
 
   executeSassJob('watch');
 
   console.log('Theme theme watch completed.');
 }
+
+main();
